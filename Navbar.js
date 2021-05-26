f180b0997a11e320ac2383e532b5d2ca4362e6dd
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-26 11:33:09 -0500

    Navbar.js
    Initial commit

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>LJS Homes</h1>
+      <div classNames="links">
+        <a href="/">Home</a>
+        <a href="/create">Houses</a>
+      </div>
+    </nav>
+  );
+
+}
+export default Navbar;
