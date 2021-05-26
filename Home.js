02c72a275fe88f28c2fc4956fe180d35d40c4bed
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-26 11:31:57 -0500

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,13 @@
+const Home= () => {
+  return (
+    <nav className="home">
+      <h1>LJS Homes</h1>
+      <div classNames="links">
+        <a href="/">Home</a>
+        <a href="/create">Houses</a>
+      </div>
+    </nav>
+  );
+
+}
+export default Home;
