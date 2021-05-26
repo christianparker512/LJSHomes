52b34cd409ac990477ba8cb7462905927c70cb1b
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-26 16:18:31 -0500

    useState Hook functionality added

--- Home.js
+++ Home.js
@@ -1,16 +1,22 @@
-consconst Home= () => {
+import {useState} from 'react';
+
+const Home= () => {
+const [name, setName]= useState('Chris');
+const [age, setAge] = useState(25);
 
   const handleClick = () => {
-    console.log("hello ninjas");
+    setName('Lisa')
+    setAge(30);
   }
-  const handleClickAgain = (name, e) => {
-    console.log("hello" + name, e.target);
-  }
+  // const handleClickAgain = (name, e) => {
+  //   console.log("hello" + name, e.target);
+  // }
   return (
       <div className="home">
       <h1>LJS Homes</h1>
+      <p>{name}</p>
         <button onClick={handleClick}>Click me</button>
-        <button onClick={(e) => handleClickAgain('Lisa', e)}>Click me again</button>
+        {/* <button onClick={(e) => handleClickAgain('Lisa', e)}>Click me again</button> */}
       </div>
   );
 }
