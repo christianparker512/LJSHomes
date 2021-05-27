e39df5c5314c327cfb084e5285b761a9404b5774
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-27 10:15:45 -0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,21 +1,17 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
+
 
 const Home= () => {
 const [blogs, setBlogs]= useState([
   {title: 'LJS Homes', body: 'Making Broken Homes Better', author: 'lisa', id: 1},
-  {title: '46 westerleigh', body: '4400 square feet', author: 'lisa', id: 1},
-  {title: '342 waxwood', body: '3/1 1100 square feet', author: 'chris', id: 2},
+  {title: '46 westerleigh', body: '4400 square feet', author: 'Black Chicken', id: 2},
+  {title: '342 waxwood', body: '3/1 1100 square feet', author: 'chris', id: 3},
 ]);
   
   return (
       <div className="home">
-        {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>Design by </p>
-            </div>
-
-        ))}
+        <BlogList blogs={blogs} title="all Homes"/>
       </div>
   );
 }
