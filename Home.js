c494d1268d61a19f436a37025ba67bd79d2f9e92
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-27 10:36:17 -0500

    Reusing Components

--- Home.js
+++ Home.js
@@ -5,13 +5,14 @@ import BlogList from './BlogList';
 const Home= () => {
 const [blogs, setBlogs]= useState([
   {title: 'LJS Homes', body: 'Making Broken Homes Better', author: 'lisa', id: 1},
-  {title: '46 westerleigh', body: '4400 square feet', author: 'Black Chicken', id: 2},
+  {title: '46 westerleigh', body: '4400 square feet', author: 'lisa', id: 2},
   {title: '342 waxwood', body: '3/1 1100 square feet', author: 'chris', id: 3},
 ]);
   
   return (
       <div className="home">
         <BlogList blogs={blogs} title="all Homes"/>
+        <BlogList blogs={blogs.filter((blog)=> blog.author ==='lisa')} title="Current Project"/>
       </div>
   );
 }
