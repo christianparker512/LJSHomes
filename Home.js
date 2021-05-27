f759c5bb38265e68cd167d854ded53f842c7e61d
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-27 15:35:51 -0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,24 +1,29 @@
-import {useState, useEffect} from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
-const Home= () => {
-  
-const [blogs, setBlogs]= useState(null);
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-  useEffect(()=>{
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-      return res.json()
-    })
-    .then(data=>{
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
         setBlogs(data);
-    })
-  }, []);
+      })
+    }, 1000);
+  }, [])
+
   return (
-      <div className="home">
-        {blogs && <BlogList blogs={blogs} title="all Homes" />}
-      </div>
+    <div className="home">
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} />}
+    </div>
   );
 }
-
+ 
 export default Home;
