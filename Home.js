c424e54062d44743d510c336dcbb8d43a02597a4
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-27 15:13:21 -0500

    removal of handleDelete

--- Home.js
+++ Home.js
@@ -1,28 +1,22 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
-
 const Home= () => {
-const [blogs, setBlogs]= useState([
-  {title: 'LJS Homes', body: 'Making Broken Homes Better', author: 'lisa', id: 1},
-  {title: '46 westerleigh', body: '4400 square feet', author: 'lisa', id: 2},
-  {title: '342 waxwood', body: '3/1 1100 square feet', author: 'chris', id: 3},
-]);
-const [name, setName] = useState('Parker');
-const handleDelete = (id) =>{
-    const newBlogs = blogs.filter(blog => blog.id !==id);
-    setBlogs(newBlogs);
-}
+  
+const [blogs, setBlogs]= useState(null);
+
   useEffect(()=>{
-    console.log("use effect ran");
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      return res.json()
+    })
+    .then(data=>{
+        setBlogs(data);
+    })
+  }, []);
   return (
       <div className="home">
-        <BlogList blogs={blogs} title="all Homes" handleDelete={handleDelete} />
-        <button onClick={()=> setName('Abby')}>Change Name</button>
-        <p>{name}</p>
-    
+        {blogs && <BlogList blogs={blogs} title="all Homes" />}
       </div>
   );
 }
