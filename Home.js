e63110ca71330986a61deb0f1be546fb51b68181
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-27 13:05:19 -0500

    useEffect Dependencies

--- Home.js
+++ Home.js
@@ -8,18 +8,21 @@ const [blogs, setBlogs]= useState([
   {title: '46 westerleigh', body: '4400 square feet', author: 'lisa', id: 2},
   {title: '342 waxwood', body: '3/1 1100 square feet', author: 'chris', id: 3},
 ]);
+const [name, setName] = useState('Parker');
 const handleDelete = (id) =>{
     const newBlogs = blogs.filter(blog => blog.id !==id);
     setBlogs(newBlogs);
 }
   useEffect(()=>{
     console.log("use effect ran");
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
   return (
       <div className="home">
-        <BlogList blogs={blogs} title="all Homes"/>
-        <BlogList blogs={blogs.filter((blog)=> blog.author ==='lisa')} title="Current Project"/>
+        <BlogList blogs={blogs} title="all Homes" handleDelete={handleDelete} />
+        <button onClick={()=> setName('Abby')}>Change Name</button>
+        <p>{name}</p>
+    
       </div>
   );
 }
