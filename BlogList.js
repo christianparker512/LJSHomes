bc8ea9a9252ca8bf9bb6a8b064ed8793cbe616a2
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-27 09:51:37 -0500

    BlogList

---
+++ BlogList.js
@@ -0,0 +1,13 @@
+const BlogList = () => {
+  return (
+    <div clasName="blog-list">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default BlogList;
