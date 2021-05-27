bcde1c4c13d48ef76afc09e0944492ec4ea8d224
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-27 15:11:44 -0500

    updated bloglist without handledelete

--- BlogList.js
+++ BlogList.js
@@ -1,15 +1,12 @@
-const BlogList = (props) => {
-  const blogs = props.blogs;
-  const title = props.title;
-
- 
-  return (
+const BlogList = ({blogs,title}) => {
+   return (
     <div clasName="blog-list">
     <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>written by {blog.author}</p>
+          {/* <button onClick={() => handleDelete(blog.id)}>Delete House</button> */}
         </div>
       ))}
     </div>
