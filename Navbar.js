1b6e02dcf3b4f96d1bc0ce6ab18bb3e22f80b56f
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-28 09:44:01 -0500

    Link tags added to navbar

--- Navbar.js
+++ Navbar.js
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>LJS Homes</h1>
-      <div classNames="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{
-          color: "white",
-          backgroundColor: 'blue',
-          borderRadius: '8px'
-        }}>New House</a>
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New House</Link>
       </div>
     </nav>
   );
-
 }
+ 
 export default Navbar;
