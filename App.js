afe89e23105f105408946e5a36beaa5b8c476073
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-28 09:08:18 -0500

    give access to the router

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from "./Navbar"
-import Home from "./Home"
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  const title = 'Welcome to LJS Homes';
-  const likes = 50;
-  const link ="http://www.hgtv.com"
-  
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-        <p>Liked {likes} times</p>
-        <p>{2021}</p>
-        <p>{"Making broken homes happy"}</p>
-        <p>{"Establish 2010"}</p>
-        <p>{Math.random()}</p>
-        <a href={link}>her site</a>
-
-        
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
+
 export default App;
